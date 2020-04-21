1135361f998029ebcdc0c9ca6ffbbe9ed64dbac1
Archana Anand <archana.anand53@gmail.com>
2020-04-21 20:34:50 +0530

    test

---
+++ src/abc/TestCase4.java
@@ -0,0 +1,24 @@
+package abc;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TestCase4 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> fruits  = new ArrayList<>();
+		fruits.add("Apple");
+		fruits.add("Mango");
+		fruits.add("Bananssa");
+		
+		Iterator<String> it = fruits.iterator();
+		while(it.hasNext()) {
+			
+			System.out.println(it.next());
+		}
+	}
+
+}
